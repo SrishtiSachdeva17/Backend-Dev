6e8148c14d11a7db03c810d0c2dd59c56a9c82ca
SrishtiSachdeva17 <17.srish@gmail.com>
2026-01-23 23:32:32 +0530

    question-1

---
+++ node.js
@@ -0,0 +1,19 @@
+
+const fs = require("fs");
+
+
+fs.readFile("log.txt", "utf-8", (error, data) => {
+    if (error) {
+        console.log("Error reading the file");
+        return;
+    }
+
+   
+    const words = data.trim().split(" ");
+    const wordCount = words.length;
+
+    
+    fs.writeFile("wordcount.txt", `Total words: ${wordCount}`, (error) => {
+        console.log("Word count written successfully!");
+    });
+});
