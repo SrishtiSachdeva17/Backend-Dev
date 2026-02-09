3061be717900f07e78c5a4f22b33071e41722d84
SrishtiSachdeva17 <17.srish@gmail.com>
2026-02-10 00:08:00 +0530

    [srishti] add the file

--- app.js
+++
@@ -1,6 +0,0 @@
-import registerBooks from "./src/registerBooks.js";
-console.log(registerBooks("Days at the Morisaki Bookshop","Satoshi Yagisawa","Rs.270"));
-import registerMember from "./src/registerMember.js";
-console.log(registerMember("Srishti","Gold"));
-import borrowRecord from "./src/borrowRecord.js";
-console.log(borrowRecord("2026-01-30T12:11:02.740Z","2026-01-30T12:09:32.497Z",2));
